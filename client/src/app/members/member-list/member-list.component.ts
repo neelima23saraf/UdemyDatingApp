d46f3595ed257176bef1821c53ed77a5d7b01e24
neelima23saraf <neelima23saraf@outlook.com>
2022-04-19 22:30:59 -0400

    End of section 10

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 
@@ -8,14 +9,16 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-members: Member[];
+members$: Observable<Member[]>;
 
   constructor(private memberService: MembersService) { }
 
   ngOnInit(): void {
-    this.loadMembers();
+    //this.loadMember()
+    this.members$ = this.memberService.getMembers();
   }
   
+  /*
   loadMembers(){
     this.memberService.getMembers().subscribe(
       members => {
@@ -23,5 +26,5 @@ members: Member[];
       }
     )
   }
-
+*/
 }
